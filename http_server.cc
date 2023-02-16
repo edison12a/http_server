4f905a32327cfa19f87eb463aec9f5db99f82827
Edison <abahedison@gmail.com>
2023-02-15 19:40:36 -0500

    add imports

--- http_server.cc
+++ http_server.cc
@@ -1,3 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdarg>
+#include <sstream>
+#include <any>
+#include <regex>
+#include <time.h>
+#include <functional>
+#include <unordered_map>
+#include <unordered_set>
+#include <csignal>
+
+#include <openssl/sha.h>
+#include <Poco/Data/RecordSet.h>
+#include <Poco/Data/Row.h>
+#include <Poco/Data/Session.h>
+#include <Poco/Data/SessionFactory.h>
+#include <Poco/Data/MySQL/MySQL.h>
+#include <Poco/Data/MySQL/Connector.h>
+#include <Poco/Data/MetaColumn.h>
+
 #include <Poco/Net/ServerSocket.h>
 #include <Poco/Net/HTTPServer.h>
 #include <Poco/Net/HTTPRequestHandler.h>
@@ -5,20 +27,41 @@
 #include <Poco/Net/HTTPResponse.h>
 #include <Poco/Net/HTTPServerRequest.h>
 #include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/Net/HTMLForm.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPCookie.h>
+#include <Poco/Net/FilePartSource.h>
+#include <Poco/Net/MailMessage.h>
+#include <Poco/Net/MailRecipient.h>
+#include <Poco/Net/SecureSMTPClientSession.h>
+#include <Poco/Net/StringPartSource.h>
+#include <Poco/Net/SSLManager.h>
+#include <Poco/Net/KeyConsoleHandler.h>
+#include <Poco/Net/ConsoleCertificateHandler.h>
+
+#include <Poco/StreamCopier.h>
+#include <Poco/Dynamic/Var.h>
+#include <Poco/JSON/Object.h>
+#include <Poco/JSON/Array.h>
+#include <Poco/JSON/Stringifier.h>
+#include <Poco/JSON/Parser.h>
+#include <Poco/URI.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Exception.h>
+#include <Poco/Path.h>
+
 #include <Poco/Util/ServerApplication.h>
-#include <iostream>
-#include <string>
-#include <vector>
+#include <Poco/Util/MapConfiguration.h>
+#include <Poco/Util/LayeredConfiguration.h>
+#include <Poco/Util/PropertyFileConfiguration.h>
+#include <Poco/AutoPtr.h>
+#include <Poco/Logger.h>
 
-#include <openssl/sha.h>
+#include <Poco/JWT/Token.h>
+#include <Poco/JWT/Signer.h>
 
-#include <aws/core/Aws.h>
-#include <aws/s3/S3Client.h>
-#include <aws/s3/model/PutObjectRequest.h>
-#include <aws/s3/model/GetObjectRequest.h>
-
-// #include <sw/redis++/redis++.h>
-// #include <podofo/podofo.h>
+#include <podofo/podofo.h>
+#include <sw/redis++/redis++.h>
 
 using namespace Poco::Net;
 using namespace Poco::Util;
